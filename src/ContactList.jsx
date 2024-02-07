2829939c581794076264944160a5dc1b476a6f62
Natasha Gjorgieva <gorgieva66@gmail.com>
2024-02-07 17:46:45 +0100

    New changes

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 const ShowContacts = ({ contacts }) => {
-  
   return (
     <div>
-      {!contacts.length && <p>Nema kontakti</p>}
+      {!contacts.length && <p>Nema kontakta</p>}
       {contacts.length > 0 && (
         <div>
           <p>Ime i prezime</p>
           <ul>
             {contacts.map((contact, index) => (
-              <li key={index}>{contact.name}</li>
+              <li key={index}>
+                <ul><p>
+                  Ime i prezime: {contact.name}
+                </p></ul>
+                <ul>
+                  <li>Telefonski broj: {contact.number}</li>
+                </ul>
+              </li>
             ))}
           </ul>
         </div>
